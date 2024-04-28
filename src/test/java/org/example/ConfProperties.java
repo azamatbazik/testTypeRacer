a7a81575617393c99845067c6a1811d9e0d4ce85
azamatbazik <bazarbai22.09@gmail.com>
2024-04-28 19:09:49 +0500

    test commit

---
+++ src/test/java/org/example/ConfProperties.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfProperties {
+
+    protected static FileInputStream fileInputStream;
+    protected static Properties PROPERTIES;
+
+    static {
+        try {
+            // Указания пути до файла с данными
+            fileInputStream = new FileInputStream("C:\\Users\\Gaming Store\\IdeaProjects\\seleniumProject\\src\\test\\resources\\conf.properties");
+            PROPERTIES = new Properties();
+            PROPERTIES.load(fileInputStream);
+            // обработка испключений в виде отсутствия пути и файлов в данном пути
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            if(fileInputStream != null){
+                try {
+                    fileInputStream.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    public static String getProperties(String key){
+        return PROPERTIES.getProperty(key);
+    }
+}
